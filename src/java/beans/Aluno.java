3cbfe43f2589d30678a6c227cb2133a6498fafac
Thiago Correia <nathimartins_@uni9.edu.br>
2020-11-21 15:25:44 -0300

    alunos

--- src/java/beans/Aluno.java
+++ src/java/beans/Aluno.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package beans;
 
-/**
- *
- * @author Correia Martins
- */
-public class Aluno {
+import java.io.Serializable;
+/*
+* @author Correia Martins
+*/
+public class Aluno implements Serializable {
+    // criando os atributos do objeto
+    private int id;
+    private String ra;
+    private String nome;
+    private String curso;
     
+    public Aluno(){
+    }
+    public int getid(){
+       return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public String getRa(){
+       return ra;
+    }
+    public void setRa(String ra){
+        this.ra = ra;
+    }
+    public String getNome(){
+       return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getCurso(){
+       return curso;
+    }
+    public void setCurso(String curso){
+        this.curso = curso;
+    }
+  
 }
