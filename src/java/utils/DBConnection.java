9390693d71a42fe01ac7e193f10a9a30f4b9e393
Thiago Correia <nathimartins_@uni9.edu.br>
2020-11-21 15:15:11 -0300

    Conexao

--- src/java/utils/DBConnection.java
+++ src/java/utils/DBConnection.java
@@ -1,14 +1,59 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
  * @author Correia Martins
  */
 public class DBConnection {
-    
+    // configurações para conexão com o bando de dados MySQL
+    private final String driver = "com.mysql.jdbc.Driver";
+    private final String url = "jdbc:mysql://localhost:3306/nomedobanco";
+    // credenciais de acesso
+    private final String usuario = "root";
+    private final String senha = "";
+    // variável para armazenar a conexão com o banco de dados
+    private static DBConnection conexao = null;
+    /**
+     * Método que prepara a configuração para a conexão
+     *
+     * @throws SQLException
+     */
+    private DBConnection() throws SQLException{
+        try{
+            Class.forName(driver);
+        }catch(ClassNotFoundException e){
+            throw new SQLException("driver");
+        }
+    }
+    /**
+     * Realiza a conexão, caso tenha sucesso
+     *
+     * @return Connection
+     * @throws SQLException
+     */
+    public Connection getConnection() throws SQLException {
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(url, usuario, senha);
+        }catch(SQLException e){
+            throw new SQLException(e.getMessage());
+        }   
+        return conn;
+    }
+     /**
+     * Retorna uma instância da conexão para utilização (Singleton)
+     *
+     * @return Connection
+     * @throws SQLException
+     */
+    public static DBConnection getInstence() throws SQLException{
+    if (conexao == null){
+        conexao = new DBConnection();
+    }
+    return conexao;
+    }
 }
